daa8634a51e207eb836eb5773dcd369907cd916c
Gustavo Pals <gustavoandrepals@gmail.com>
2022-12-12 22:02:16 -0300

    feat: validações com class-validator

---
+++ src/create-notification-body.ts
@@ -0,0 +1,13 @@
+import { IsNotEmpty, IsUUID } from 'class-validator';
+import { Length } from 'class-validator/types/decorator/decorators';
+
+export class CreateNotificationBody {
+  @IsNotEmpty()
+  @IsUUID()
+  recepientId: string;
+  @IsNotEmpty()
+  @Length(5, 240)
+  content: string;
+  @IsNotEmpty()
+  category: string;
+}
