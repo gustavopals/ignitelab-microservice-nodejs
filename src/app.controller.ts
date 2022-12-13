569fa0e64e01b5a911990721463ea7cbbdc6f3bb
Gustavo Pals <gustavoandrepals@gmail.com>
2022-12-12 21:31:41 -0300

    feat: create notification

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { randomUUID } from 'crypto';
 import { PrismaService } from './prisma.service';
 
 @Controller('notifications')
@@ -8,5 +9,17 @@ export class AppController {
   @Get()
   list() {
     return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create() {
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content: 'Você tem uma nova solcitação de amizade!',
+        category: 'social',
+        recepientId: randomUUID(),
+      },
+    });
   }
 }
